dca929975b5f6ea081e13aa949780dc1ad2cb3be
Yoav Ganbar <yoav.ganbar@gmail.com>
2021-04-10 22:22:00 +0300

    Initial commit

---
+++ src/Button.tsx
@@ -0,0 +1,23 @@
+// src/Button.tsx
+
+import React, { FC } from 'react';
+
+export interface ButtonProps {
+  text: string;
+}
+
+export const Button: FC<ButtonProps> = ({ text }) => {
+  return (
+    <button
+      style={{
+        backgroundColor: 'blueviolet',
+        color: 'white',
+        fontSize: '17px',
+        padding: '10px 16px',
+        borderRadius: '99999px',
+      }}
+    >
+      {text}
+    </button>
+  );
+};
