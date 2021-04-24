8d9d09af8c26fb9742329707da172c468d0dd246
Yoav Ganbar <yoav.ganbar@gmail.com>
2021-04-24 11:09:07 +0300

    Fix example project by pinning parcel version. Change Demo component and export all components from index file. Rename default componet to Demo. use index file to export wanted components. fix yarn.lock file.

--- example/index.tsx
+++ example/index.tsx
@@ -2,11 +2,13 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Thing } from '../.';
+import { Button } from '../.';
 
 const App = () => {
   return (
     <div>
       <Thing />
+      <Button text="test" />
     </div>
   );
 };
